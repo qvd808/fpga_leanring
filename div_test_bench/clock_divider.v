19b314805cfafd3815b963e481fb4e6056b9aa03
Quang Vinh Dang <dqvinh101@gmail.com>
2025-08-16 19:08:39 -0700

    Test bench

---
+++ div_test_bench/clock_divider.v
@@ -0,0 +1,40 @@
+// Clock divider
+
+module clock_divider #(
+    // parameters
+    parameter                 COUNT_WIDTH = 24,
+    parameter [COUNT_WIDTH:0] MAX_COUNT   = 6000000 - 1
+
+) (
+    // Inputs
+    input clk,
+    input rst,
+
+    //Outputs
+    output reg out
+
+);
+
+  // Parameters -- Old ways
+  // parameter COUNT_WIDTH = 24,
+  // parameter [COUNT_WIDTH:0] MAX_COUNT = 6000000 - 1
+
+
+  // Internal signals
+  reg div_clk;
+  reg [COUNT_WIDTH:0] count;
+
+  // Clock divicer
+  always @(posedge clk or posedge rst) begin
+    if (rst == 1'b1) begin
+      count <= 0;
+      out   <= 0;
+    end else if (count == MAX_COUNT) begin
+      count <= 0;
+      out   <= ~out;
+    end else begin
+      count <= count + 1;
+    end
+  end
+
+endmodule
